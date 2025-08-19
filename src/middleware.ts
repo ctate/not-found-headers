42f01fb235d1be9bef57e7e882a2b4270ddcb0ed
dks333 <kaishan333sam@gmail.com>
2025-08-19 10:52:58 -0700

    fail: not-found is not working with dynamic set to force-static

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import { headers } from "next/headers";
 export function middleware(req: NextRequest) {
   const currentHost = req.headers.get("host") || "unknown";
   const url = req.nextUrl.clone();
-  url.pathname = "/";
+  url.pathname = "/test/multitenant";
   const response = NextResponse.rewrite(url);
   response.headers.set("x-site-host", currentHost);
   return response;
