d6da281c6b34669b78fbf488ec795c98ba3d6047
aldosch <git@aldo.io>
2025-08-19 13:20:07 +1000

    init

---
+++ src/middleware.ts
@@ -0,0 +1,11 @@
+import { NextResponse, NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+export function middleware(req: NextRequest) {
+  const currentHost = req.headers.get("host") || "unknown";
+  const url = req.nextUrl.clone();
+  url.pathname = "/";
+  const response = NextResponse.rewrite(url);
+  response.headers.set("x-site-host", currentHost);
+  return response;
+}
